0e581a9bd3d54700fdbd649da62596560e050021
onurbilenlacin <73442869+onurbilenlacin@users.noreply.github.com>
2024-04-30 22:17:03 +0300

    project-initialized

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    purge: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans]
+            }
+        }
+    },
+    plugins: []
+};
